0f89adff66ac1fa5c9a94f74acf6d7b16a919f0d
nour_araar <nour_araar_2009@yahoo.com>
2022-02-12 19:07:00 +0800

    refactor data source test into groups

--- test/weather_data_source_test.dart
+++ test/weather_data_source_test.dart
@@ -20,59 +20,63 @@ void main() {
 
     dataSource = WeatherDataSourceImpl(apiClient: apiClient);
   });
+  group(
+    'get /weather',
+    () {
+      test(
+        'should perform a GET request on /weather?lat=3.14&lon=101.69',
+        () async {
+          // arrange
+          when(
+            () => apiClient.getWeatherByLatAndLng(3.14, 101.69),
+          ).thenAnswer(
+            (_) async => _mockWeather,
+          );
 
-  test(
-    'should perform a GET request on /weather?lat=3.14&lon=101.69',
-    () async {
-      // arrange
-      when(
-        () => apiClient.getWeatherByLatAndLng(3.14, 101.69),
-      ).thenAnswer(
-        (_) async => _mockWeather,
+          // act
+          dataSource.getWeatherByLatAndLng(3.14, 101.69);
+          // assert
+          verify(() => apiClient.getWeatherByLatAndLng(3.14, 101.69));
+          verifyNoMoreInteractions(apiClient);
+        },
+      );
+      test(
+        'should return Weather() when get response /weather?lat=3.14&lon=101.69',
+        () async {
+          when(
+            () => apiClient.getWeatherByLatAndLng(3.14, 101.69),
+          ).thenAnswer(
+            (_) async => _mockWeather,
+          );
+
+          final response = await dataSource.getWeatherByLatAndLng(3.14, 101.69);
+
+          expect(response, _mockWeather);
+        },
       );
 
-      // act
-      dataSource.getWeatherByLatAndLng(3.14, 101.69);
-      // assert
-      verify(() => apiClient.getWeatherByLatAndLng(3.14, 101.69));
-      verifyNoMoreInteractions(apiClient);
-    },
-  );
-  test(
-    'should return Weather() when get response /weather?lat=3.14&lon=101.69',
-    () async {
-      when(
-        () => apiClient.getWeatherByLatAndLng(3.14, 101.69),
-      ).thenAnswer(
-        (_) async => _mockWeather,
-      );
-
-      final response = await dataSource.getWeatherByLatAndLng(3.14, 101.69);
-
-      expect(response, _mockWeather);
-    },
-  );
-
-  test(
-    'should throw an Exception when the response code is 404 or other (unsuccess)',
-    () async {
-      // arrange
-      when(() => apiClient.getWeatherByLatAndLng(3.14, 101.69)).thenThrow(
-        DioError(
-          response: Response(
-            data: 'Something went wrong',
-            statusCode: 404,
-            requestOptions: RequestOptions(path: ''),
-          ),
-          requestOptions: RequestOptions(path: ''),
-        ),
-      );
-      // act
-      final call = dataSource.getWeatherByLatAndLng;
-      // assert
-      expect(
-        () => call(3.14, 101.69),
-        throwsA(const TypeMatcher<ServerException>()),
+      test(
+        'should throw an Exception when the response code is 404 or other (unsuccess)',
+        () async {
+          // arrange
+          when(() => apiClient.getWeatherByLatAndLng(3.14, 101.69)).thenThrow(
+            DioError(
+              response: Response(
+                data: 'Something went wrong',
+                statusCode: 404,
+                requestOptions: RequestOptions(path: ''),
+              ),
+              requestOptions: RequestOptions(path: ''),
+            ),
+          );
+          // act
+          final call = dataSource.getWeatherByLatAndLng;
+          // assert
+          expect(
+            () => call(3.14, 101.69),
+            throwsA(const TypeMatcher<ServerException>()),
+          );
+        },
       );
     },
   );
