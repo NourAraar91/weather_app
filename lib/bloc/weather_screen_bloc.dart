7adec81acdad152f727f7003f6ce827bc1c2b5d2
nour_araar <nour_araar_2009@yahoo.com>
2022-02-12 21:18:23 +0800

    get weather bloc initial state

--- lib/bloc/weather_screen_bloc.dart
+++ lib/bloc/weather_screen_bloc.dart
@@ -1,12 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/dataSource/weather_data_source.dart';
 import 'package:weather_app/models/city.dart';
 
-class WeatherScreenBloc {
+class WeatherScreenBloc extends Cubit<WeatherBlocState> {
   final WeatherDataSource dataSource;
   final City city;
-  
+
   WeatherScreenBloc({
     required this.dataSource,
     required this.city,
-  });
+  }) : super(WeatherBlocInitialState());
 }
+
+abstract class WeatherBlocState {}
+
+class WeatherBlocInitialState implements WeatherBlocState {}
--- test/weather_screen_bloc_test.dart
+++ test/weather_screen_bloc_test.dart
@@ -1,8 +1,10 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:weather_app/bloc/weather_screen_bloc.dart';
 import 'package:weather_app/dataSource/weather_data_source.dart';
 import 'package:weather_app/models/city.dart';
+import 'package:weather_app/models/weather.dart';
 
 import 'weather_data_source_test.dart';
 
@@ -14,6 +16,8 @@ void main() {
 
   City _kualaLumpur = City(lat: 3.1478, lon: 101.6953);
 
+  CurrentWeather _currentWeather = CurrentWeather();
+
   setUp(() async {
     apiClient = MockAPIClient();
     dataSource = WeatherDataSourceImpl(apiClient: apiClient);
@@ -24,8 +28,12 @@ void main() {
     );
   });
 
-  test('test city coords', () {
+  test('bloc should have city with lat and lon', () {
     expect(weatherScreenBloc.city.lat, _kualaLumpur.lat);
     expect(weatherScreenBloc.city.lon, _kualaLumpur.lon);
+  });
+
+  test('should have initial state as [WeatherBlocInitialState]', () {
+    expect(weatherScreenBloc.state.runtimeType, WeatherBlocInitialState);
   });
 }
